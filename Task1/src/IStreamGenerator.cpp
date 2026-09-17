ce3e081f54ec01ec4620c9593fc43d53feb60db2
Голованов ТКИ242 <129624626+GeorgiyGolovanov@users.noreply.github.com>
2026-09-17 20:50:58 +0300

    Update IStreamGenerator.cpp

--- Task1/src/IStreamGenerator.cpp
+++ Task1/src/IStreamGenerator.cpp
@@ -1,12 +1,15 @@
 #include "../include/IStreamGenerator.h"
 
-miit::algebra::IStreamGenerator::IStreamGenerator(std::istream& in) : in{ in }
+namespace miit::algebra
 {
-}
+	IStreamGenerator::IStreamGenerator(std::istream& in) : in(in)
+	{
+	}
 
-int miit::algebra::IStreamGenerator::generate()
-{
-	int value = 0;
-	this->in >> value;
-	return value;
+	int IStreamGenerator::generate() const
+	{
+		int value = 0;
+		this->in >> value;
+		return value;
+	}
 }
