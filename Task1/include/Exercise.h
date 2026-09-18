4726a21cdbfc0b42088af7b0846cf0131825d174
Голованов ТКИ242 <129624626+GeorgiyGolovanov@users.noreply.github.com>
2026-09-18 18:05:27 +0300

    Update Exercise.h

--- Task1/include/Exercise.h
+++ Task1/include/Exercise.h
@@ -35,11 +35,6 @@ namespace miit::algebra
 		virtual ~Exercise() = default;
 
 		/**
-		 * @brief Заполнить матрицу с помощью выбранного генератора
-		 */
-		void Fill();
-
-		/**
 		 * @brief Выполнить задание
 		 * @return Матрица с результатом задания
 		 */
